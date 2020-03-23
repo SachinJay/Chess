e4ee06810ef2f3fcdff2af881eadc2e0633b439a
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-23 13:16:24 -0400

    Implemented part of Square class
    -Instead of just having a Piece object associated with a square, there
    is a Piece Option, that way empty squares won't need null checking

---
+++ Chess/src/Pawn.java
@@ -0,0 +1,21 @@
+/**
+ * Class to represent a pawn
+ * @author Sachin Devasahayam
+ *
+ */
+public class Pawn extends Piece
+{
+
+	public Pawn(Side side)
+	{
+		super(side);
+	}
+
+	@Override
+	public Boolean canMove(Board board, Square start, Square end)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- Chess/src/Square.java
+++ Chess/src/Square.java
@@ -1,3 +1,5 @@
+import java.util.Optional;
+
 /**
  * 
  * @author Sachin Devasahayam
@@ -8,6 +10,35 @@ public class Square
 {
 	//A square has only two things, a position and a piece
 	private Position pos; 
-	private Piece piece; 
+	private Optional<Piece> piece;
+	
+	/**
+	 * Default constructor, creates the a1 square with no piece on it
+	 */
+	public Square()
+	{
+		pos = new Position();
+		piece = Optional.ofNullable(null);
+	}
+	
+	/**
+	 * Creates a square at given position with given piece on it
+	 * @param pos the position of the square
+	 * @param piece the piece option that is on this square. Optional of null if the square is blank
+	 */
+	public Square(Position pos, Optional<Piece> piece)
+	{
+		this.pos = pos; 
+		this.piece = piece;
+	}
+	
+	/**
+	 * 
+	 * @return true if there is no piece on this square
+	 */
+	public boolean isEmpty()
+	{
+		return this.piece.isPresent();
+	}
 
 }
