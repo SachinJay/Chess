5d3fd57321f54fc508de596545533cf8e8c65298
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-24 13:42:22 -0400

    Implemented Rook's canMove() method

--- Chess/src/Rook.java
+++ Chess/src/Rook.java
@@ -11,11 +11,24 @@ public class Rook extends Piece
 	@Override
 	public Boolean canMove(Board board, Square start, Square end)
 	{
-		int changeInR = start.getPos().getRow() - end.getPos().getRow() ;
-		int changeInC = start.getPos().getCol() - end.getPos().getCol() ;
+		//Start and end row numbers
+		int rStart = start.getPos().getRow();
+		int rEnd = end.getPos().getRow();
 		
-//		Boolean rBackwards = 
+		//Start and end column numbers
+		int cStart = start.getPos().getCol();
+		int cEnd = end.getPos().getCol();
 		
+		int changeInR = rStart - rEnd;
+		int changeInC = cStart - cEnd;
+		
+		Boolean rBackwards = changeInR < 0;
+		Boolean rForwards = changeInR >= 0;
+		
+		Boolean cBackwards = changeInC < 0;
+		Boolean cForwards = changeInC >= 0;
+		
+		//TODO do I ever need to use this absolute value? I don't think so, maybe delete this
 		changeInR = Math.abs(changeInR);
 		changeInC = Math.abs(changeInC);
 		
@@ -33,7 +46,38 @@ public class Rook extends Piece
 			
 			//Rook could be moving forwards or backwards in a row or column, need to specify
 			
+			//Set whether we increment or decrement
+			int rInc = rForwards? 1 : -1;
+			int cInc = cForwards? 1 : -1;
 			
+			if(changeInR == 0)
+			{
+				for(int c = cStart + cInc; c < cEnd; c+=cInc)
+				{
+					Position curPos = new Position(c,rStart);
+					if(!board.getSquare(curPos).isEmpty())
+					{
+						return false;
+					}
+				}
+			}
+			else
+			{
+				for(int r = rStart + rInc; r < rEnd; r+=rInc)
+				{
+					return false;
+				}
+			}
+			
+			if(end.isEmpty())
+			{
+				return true;
+			}
+			else
+			{
+				Boolean opSide = !(this.getSide().equals(end.getPiece().getSide()));
+				return opSide;
+			}
 			
 		}
 	}
