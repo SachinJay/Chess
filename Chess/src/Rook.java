f0671866faa9e2e215c277cb727bd90bf4c84d7d
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-24 13:10:20 -0400

    Began implementing rook's canMove method

--- Chess/src/Rook.java
+++ Chess/src/Rook.java
@@ -11,8 +11,31 @@ public class Rook extends Piece
 	@Override
 	public Boolean canMove(Board board, Square start, Square end)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		int changeInR = start.getPos().getRow() - end.getPos().getRow() ;
+		int changeInC = start.getPos().getCol() - end.getPos().getCol() ;
+		
+//		Boolean rBackwards = 
+		
+		changeInR = Math.abs(changeInR);
+		changeInC = Math.abs(changeInC);
+		
+		Boolean pathIsStraightLine = changeInR !=0 && changeInC != 0;
+		
+		if(!pathIsStraightLine)
+		{
+			return false;
+		}
+		else
+		{
+			//TODO perform check that no other pieces are in the way from start to end
+			//Obviously, color of pieces does not matter for the inbetween pieces
+			//What matters is that the in between pieces do not exist
+			
+			//Rook could be moving forwards or backwards in a row or column, need to specify
+			
+			
+			
+		}
 	}
 
 }
