ff013005a5add706d6b6cea1bdd130ff11ccff59
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-24 13:11:19 -0400

    Fixed naming conventions in Knight and King files

--- Chess/src/King.java
+++ Chess/src/King.java
@@ -13,10 +13,10 @@ public class King extends Piece
 	{
 		
 		//Only valid if change in x and change in y are both less than or equal to 1
-		int changeInX = Math.abs(start.getPos().getRow() - end.getPos().getRow() );
-		int changeInY = Math.abs(start.getPos().getCol() - end.getPos().getCol() );
+		int changeInR = Math.abs(start.getPos().getRow() - end.getPos().getRow() );
+		int changeInC = Math.abs(start.getPos().getCol() - end.getPos().getCol() );
 		
-		Boolean isValid = changeInX <=1 && changeInY <= 1 && !(changeInX == 0 && changeInY == 0);
+		Boolean isValid = changeInR <=1 && changeInC <= 1 && !(changeInR == 0 && changeInC == 0);
 		
 		return isValid && (end.isEmpty() || !end.getPiece().getSide().equals(this.getSide()));
 	}
--- Chess/src/Knight.java
+++ Chess/src/Knight.java
@@ -15,11 +15,11 @@ public class Knight extends Piece
 	@Override
 	public Boolean canMove(Board board, Square start, Square end)
 	{
-		int changeInX = Math.abs(start.getPos().getRow() - end.getPos().getRow() );
-		int changeInY = Math.abs(start.getPos().getCol() - end.getPos().getCol() );
+		int changeInR = Math.abs(start.getPos().getRow() - end.getPos().getRow() );
+		int changeInC = Math.abs(start.getPos().getCol() - end.getPos().getCol() );
 		
 		//true iff the start and end position form a valid knight shaped 'L'
-		Boolean isValidKnightMove = changeInX * changeInY == Constants.KNIGHT_PRODUCT;
+		Boolean isValidKnightMove = changeInR * changeInC == Constants.KNIGHT_PRODUCT;
 		
 		//Cannot put second part of disjunction into a variable because it could throw error
 		//If it does throw an error though, it will not be reached because this will short circuit
