313e38f27fb7535bd347269e37415624c0c3cd57
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-24 21:09:58 -0400

    Implemented Bishop class

--- Chess/src/Bishop.java
+++ Chess/src/Bishop.java
@@ -12,8 +12,50 @@ public class Bishop extends Piece
 	@Override
 	public Boolean canMove(Board board, Square start, Square end)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Position startPos = start.getPos();
+		int rStart = startPos.getRow();
+		int cStart = startPos.getCol();
+		
+		Position endPos = end.getPos();
+		int rEnd = endPos.getRow();
+		int cEnd = endPos.getCol();
+		
+		int changeInR = rStart-rEnd;
+		int changeInC = cStart - cEnd;
+		
+		Boolean pathIsDiagonal = Math.abs(changeInR) == Math.abs(changeInC);
+		
+		Boolean rForwards = changeInR >= 0;
+		Boolean cForwards = changeInC >= 0;
+		
+		int rInc = rForwards? 1 : -1;
+		int cInc = cForwards? 1 : -1;
+		
+		//Check that there is nothing on the path
+		for(int r = rStart + rInc; r < rEnd; r += rInc)
+		{
+			for(int c = cStart + cInc; c < cEnd; c +=cInc)
+			{
+				//I.e. if along the diagonal
+				if(Math.abs(r-rStart) == Math.abs(c-cStart))
+				{
+					Position curPos = new Position(c,r);
+					if(!board.getSquare(curPos).isEmpty())
+					{
+						return false;
+					}
+				}
+			}
+		}
+		
+		if(end.isEmpty())
+		{
+			return true;
+		}
+		else
+		{
+			return end.getPiece().getSide() != this.getSide();
+		}
 	}
 	
 }
