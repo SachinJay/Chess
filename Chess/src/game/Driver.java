8672dbe7fb7fe649a415d5096e556badb26dd81a
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-26 13:46:04 -0400

    Added driver file to start the game

---
+++ Chess/src/game/Driver.java
@@ -0,0 +1,15 @@
+package game;
+
+import java.io.IOException;
+
+import gui.Grid;
+
+public class Driver
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		Grid grid = new Grid();
+	}
+
+}
